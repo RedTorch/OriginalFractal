830c940624840bdb29c4221b615878733a84e487
RedTorch <kenta.medina@gmail.com>
2015-01-26 11:26:33 -0800

    Added branching system

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -1,5 +1,21 @@
-float myMagnitude = 1.1;
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
+float myMagnitude = 1.1f;
+public void setup()
 {
 	//whatever here
 	size(600,600);
@@ -9,13 +25,13 @@ void setup()
 	stroke(150,150,250);
 	noLoop();
 }
-void draw()
+public void draw()
 {
 	background(0);
 	myMagnitude = 2;
 	squares(120,300,1,20);
 }
-void squares(int x, int y, int direction, int dimensions)
+public void squares(int x, int y, int direction, int dimensions)
 {
 	//whatever here
 	if(dimensions>800)
@@ -50,4 +66,13 @@ void squares(int x, int y, int direction, int dimensions)
 		squares(x-(int)(dimensions/myMagnitude+dimensions/(myMagnitude*myMagnitude)),y-(int)(dimensions/myMagnitude+dimensions/(myMagnitude*myMagnitude)), direction+1, (int)(dimensions*myMagnitude));
 		squares(x-(int)(dimensions/myMagnitude+dimensions/(myMagnitude*myMagnitude)),y-(int)(dimensions/myMagnitude+dimensions/(myMagnitude*myMagnitude)), direction, (int)(dimensions*myMagnitude));
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
